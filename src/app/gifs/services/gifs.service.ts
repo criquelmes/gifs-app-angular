b4b4f15ca67133274c07a5ce5b282c08f4db37e6
Christopher Riquelme <ccriquelmes@gmail.com>
2025-01-24 00:35:12 -0300

    HttpClient - HttpParams

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,7 +7,9 @@ import { Injectable } from '@angular/core';
 export class GifsService {
   private _tagsHistory: string[] = [];
   private apiKey: string = 'xOFDa72rbJaTEnBJUc14bPUNumEZ71Kj';
-  constructor() {}
+  private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -24,7 +27,21 @@ export class GifsService {
 
   searchTag(tag: string): void {
     if (tag.length === 0) return;
-
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag);
+
+    this.http.get(`${this.serviceUrl}/search`, { params }).subscribe((resp) => {
+      console.log(resp);
+    });
+
+    // const resp = await fetch(
+    //   'http://api.giphy.com/v1/gifs/search?api_key=xOFDa72rbJaTEnBJUc14bPUNumEZ71Kj&q=locorene&limit=10'
+    // );
+    // const data = await resp.json();
+    // console.log(data);
   }
 }
